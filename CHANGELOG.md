f4423cca77563cf03687e543ee64d14cc02348eb
andries <andries@skrepr.com>
2021-03-14 18:09:44 +0100

    1.0.3

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,12 @@
 
 All notable changes to `form-to-json-bundle` will be documented in this file
 
+## 1.0.3 - 2020-03-14
+- Expanded schema
+
+## 1.0.2 - 2020-03-14
+- Fixed bugs in text types
+
 ## 1.0.1 - 2020-03-14
 - Fixed missing dependency for CollectionTypeTransformer
 
--- src/Transformer/BuiltIn/AbstractTypeTransformer.php
+++ src/Transformer/BuiltIn/AbstractTypeTransformer.php
@@ -21,14 +21,29 @@ abstract class AbstractTypeTransformer implements TypeTransformerInterface
     {
         $schema['id'] = $formView->vars['id'] ?? null;
         $schema['name'] = $formView->vars['name'] ?? null;
+        $schema['disabled'] = $formView->vars['disabled'] ?? null;
         $schema['label'] = $formView->vars['label'] ?? null;
+        $schema['label_format'] = $formView->vars['label_format'] ?? null;
+        $schema['label_html'] = $formView->vars['label_html'] ?? null;
+        $schema['multipart'] = $formView->vars['multipart'] ?? null;
         $schema['unique_block_prefix'] = $formView->vars['unique_block_prefix'] ?? null;
+        $schema['row_attr'] = $formView->vars['row_attr'] ?? null;
+        $schema['translation_domain'] = $formView->vars['translation_domain'] ?? null;
+        $schema['label_translation_parameters'] = $formView->vars['label_translation_parameters'] ?? null;
+        $schema['attr_translation_parameters'] = $formView->vars['attr_translation_parameters'] ?? null;
+        $schema['valid'] = $formView->vars['valid'] ?? null;
         $schema['value'] = $formView->vars['value'] ?? null;
         $schema['required'] = $formView->vars['required'] ?? null;
+        $schema['size'] = $formView->vars['size'] ?? null;
+        $schema['label_attr'] = $formView->vars['label_attr'] ?? null;
         $schema['help'] = $formView->vars['help'] ?? null;
+        $schema['help_attr'] = $formView->vars['help_attr'] ?? null;
+        $schema['help_html'] = $formView->vars['help_html'] ?? null;
+        $schema['help_translation_parameters'] = $formView->vars['help_translation_parameters'] ?? null;
         $schema['compound'] = $formView->vars['compound'] ?? null;
         $schema['method'] = $formView->vars['method'] ?? null;
         $schema['action'] = $formView->vars['action'] ?? null;
+        $schema['submitted'] = $formView->vars['submitted'] ?? null;
         $schema['attr'] = $formView->vars['attr'] ?? null;
 
         return $schema;
